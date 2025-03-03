a2f67be64800491b187fb596f6ecdeb4b46e3ce7
Viet Hoang <gvhoang30112004@gmail.com>
2025-03-03 15:19:07 +0700

    carousel init

--- src/database/data.js
+++ src/database/data.js
@@ -38,5 +38,27 @@ export const data = {
       "id": 3,
       "image": "/images/store1.svg"
     },
+  ],
+  "thumbnail" : [
+    {
+      "id" : 1,
+      "image" : "https://www.mykingdom.com.vn/cdn/shop/files/1280x496_0f861e00-cc45-4a74-8aea-25669853be6b.png?v=1740760910&width=1200",
+      "title" : "ok"
+    },
+    {
+      "id" : 2,
+      "image" : "https://www.mykingdom.com.vn/cdn/shop/files/F1_2025_Hero-banner_1280x496px_k_nut_mua_ngay_a90a7ffe-67db-4bd1-a511-44a860f69e0c.jpg?v=1740761345&width=1200",
+      "title" : "ok"
+    },
+    {
+      "id" : 3,
+      "image" : "https://www.mykingdom.com.vn/cdn/shop/files/1280x496px_28ccc7fa-c393-4a31-a349-56e6f58932db.jpg?v=1740735673&width=1200",
+      "title" : "ok"
+    },
+    {
+      "id" : 4,
+      "image" : "https://www.mykingdom.com.vn/cdn/shop/files/MKD-CTKM-Tui-Gap_1280x496px.jpg?v=1740734998&width=1200",
+      "title" : "ok"
+    },
   ]
 }
--- src/pages/Homepage/Homepage.jsx
+++ src/pages/Homepage/Homepage.jsx
@@ -1,5 +1,13 @@
+import { CarouselBox } from "@/components/CarouselBox/CarouselBox";
+import { data } from "@/database/data";
 import React from "react";
 
 export default function Homepage() {
-  return <div>Homepage</div>;
+  return (
+    <div>
+      <div className="w-full flex items-center justify-center">
+        <CarouselBox data={data.thumbnail} />
+      </div>
+    </div>
+  );
 }
