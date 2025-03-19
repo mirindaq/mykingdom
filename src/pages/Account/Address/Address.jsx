9dbe09b4a95a7e1739261a34fabd0645f27cc139
NguyenQuocHuyGITHUB <122158094+Nine99Huy8181@users.noreply.github.com>
2025-03-19 20:45:00 +0700

    delete

--- src/pages/Account/Address/Address.jsx
+++
@@ -1,8 +0,0 @@
-import AccountAddress from '@/components/AccountAddress/AccountAddress'
-import React from 'react'
-
-export default function Address() {
-  return (
-    <AccountAddress></AccountAddress>
-  )
-}
